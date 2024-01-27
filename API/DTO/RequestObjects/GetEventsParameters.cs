04337a879f66caee038bf96f580be9084a8d9f36
valetra <valetra100102@gmail.com>
2024-01-27 15:37:22 +0300

    create request DTO

---
+++ API/DTO/RequestObjects/GetEventsParameters.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RequestObjects;
+
+public class GetEventsInRangeParameters
+{
+	public DateTime From { get; set; }
+	public DateTime To { get; set; }
+}
