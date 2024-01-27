9d88e6b70319490ee1963db9625933d548821a9c
valetra <valetra100102@gmail.com>
2024-01-27 15:20:30 +0300

    add necessary to file

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+
+using Services;
+using DAL.Contexts;
+using DAL.Repositories;
+using DAL.Models;
+using Mapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
+
+string? databaseConnectionString = configuration.GetConnectionString("database");
+
+if (databaseConnectionString is null)
+{
+    string message = "Connection string \"database\" is required";
+    Console.Error.WriteLine(message);
+    return;
+}
+
+builder.Services.AddDbContext<EventContext>(options => options.UseNpgsql(databaseConnectionString));
+
+builder.Services.AddScoped<IBaseRepository<Event, Guid>, BaseRepository<Event, Guid>>(serviceProvider =>
+    new BaseRepository<Event, Guid>(serviceProvider.GetRequiredService<EventContext>()));
+
+builder.Services.AddScoped<IEventService, EventService>();
+
+builder.Services.AddAutoMapper(typeof(AppMappingProfile));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.CustomSchemaIds(type => type.ToString());
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
