25bcd8f64ac15d4496d2b1beceea61e3cc847747
valetra <valetra100102@gmail.com>
2024-01-27 17:13:29 +0300

    delete unnecessary methods

--- API/DAL/BaseRepository/IBaseRepository.cs
+++ API/DAL/BaseRepository/IBaseRepository.cs
@@ -2,9 +2,6 @@ namespace DAL.Repositories;
 
 public interface IBaseRepository<TModel, T> where TModel : BaseModel<T>
 {
-    Task<List<TModel>> GetAll();
     IQueryable<TModel> GetAllQuery();
-    Task<TModel?> Get(T id);
     Task<TModel> Create(TModel model);
-    Task<bool> Delete(T id);
 }
