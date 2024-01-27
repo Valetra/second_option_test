f11fcb6f4d94c36b1a520cbd5194924e1f5aa3fe
valetra <valetra100102@gmail.com>
2024-01-27 17:14:52 +0300

    start some logic in second method

--- API/Services/EventService/EventService.cs
+++ API/Services/EventService/EventService.cs
@@ -5,6 +5,28 @@ namespace Services;
 
 public class EventService(IBaseRepository<Event, Guid> eventRepository) : IEventService
 {
+    public async Task<List<ValuesAtMinute>> GetValuesAtMinutes(DateTime from, DateTime? to)
+    {
+        IQueryable<Event> events = eventRepository.GetAllQuery();
+
+        if (!events.Any())
+        {
+            return new List<ValuesAtMinute>();
+        }
+
+        if (to is not null)
+        {
+            IQueryable<Event> eventsInRange = events.Where(e => e.CreateDateTime >= from && e.CreateDateTime <= to);
+            //TODO:Div range by minutes
+            //TODO:Return List<ValuesAtMinute>
+        }
+
+        IQueryable<Event> eventsFrom = events.Where(e => e.CreateDateTime >= from);
+        //TODO:Div range by minutes
+        //TODO:Return List<ValuesAtMinute>
+        return new List<ValuesAtMinute>();
+    }
+
     public async Task<Event> Create(Event @event)
     {
         return await eventRepository.Create(@event);
--- API/Services/EventService/IEventService.cs
+++ API/Services/EventService/IEventService.cs
@@ -4,5 +4,6 @@ namespace Services;
 
 public interface IEventService
 {
+    Task<List<ValuesAtMinute>> GetValuesAtMinutes(DateTime from, DateTime? to);
     Task<Event> Create(Event @event);
 }
