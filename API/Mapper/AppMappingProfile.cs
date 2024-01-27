d21a6c8faa4d0b567688e1142cff18b0866ecf58
valetra <valetra100102@gmail.com>
2024-01-27 17:12:28 +0300

    add new map

--- API/Mapper/AppMappingProfile.cs
+++ API/Mapper/AppMappingProfile.cs
@@ -7,5 +7,6 @@ public class AppMappingProfile : Profile
 	public AppMappingProfile()
 	{
 		CreateMap<RequestObjects.Event, DAL.Models.Event>();
+		CreateMap<Services.ValuesAtMinute, ResponseObjects.ValuesAtMinute>();
 	}
 }
